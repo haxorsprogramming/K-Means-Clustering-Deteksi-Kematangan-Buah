d8c3d5c7c6076cfe46d0ae7f88204f7e20cc134c
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-18 21:19:33 +0700

    update

--- tes.m
+++ tes.m
@@ -2,37 +2,14 @@
 foto = imread('dataset_buah_jeruk/jeruk1.jpg');
 foto = (rgb2gray(foto));
 
-panjang = 31;
-lebar = 41;
+p_total = 124;
+l_total = 164;
 
-P = 41;
-L = 31;
-pos_p = 1;
-pos_l = 1;
-total = 0;
+P_satuan = 31;
+L_satuan = 41;
 
-for o = 1:4
-    for r = o:4
-        pos_x_p = 1;
-        pos_x_l = 1;
-        for n = pos_x_l:P
-            for c = pos_x_p:L
-            pix = foto(1,1);
-            % disp(pix);
-            HAS(n,c) = pix;
-            P = P + 41;
-            L = L + 31;
-            end_p = p + (p + 41) - 1;
-            end_l = l + (p + 31) - 1;
-            end
-        end
-        
+for q = 1:L_satuan
+    for w = q:P_satuan
+        DAR(w,q) = foto(w,q);
     end
 end
-
-
-    
-nilaiHas = sum(HAS(:));
-rataHas = round(nilaiHas / 1271);
-
-disp(num2str(rataHas));
